d2e9a13f5cdc6fbb297cc2f24262887bcf8968c5
BaoLanNguyen <nhlpool27@gmail.com>
2023-04-19 18:20:36 +0200

    Update Program.cs

--- datastrukturer/Program.cs
+++ datastrukturer/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stack myStack = new Stack();
+
+        // Push some elements onto the stack
+        myStack.Push("apple");
+        myStack.Push("banana");
+        myStack.Push("cherry");
+        myStack.Push("date");
+
+        // Pop elements off the stack
+        Console.WriteLine("Popping elements off the stack:");
+        while (myStack.Count > 0)
+        {
+            Console.WriteLine(myStack.Pop());
+        }
+    }
+}
